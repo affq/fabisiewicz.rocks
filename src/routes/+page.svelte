9d4647d00a9b5faa0f9510973518210f74c12bce
affq <adrianfabisiewicz@gmail.com>
2023-12-18 14:27:02 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+    {i}
+    <button on:click={addOne}>dodaj jeden</button>
+</div>
+
+<script>
+    let i = 0;
+    const addOne = () => {
+        i++;
+        console.log(i);
+    }
+</script>
