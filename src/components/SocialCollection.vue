e3a8a1d98553bb0cc098f3b811a2751379f2382f
adrian <adrianfabisiewicz@gmail.com>
2025-03-31 04:13:28 +0200

    -ig

--- src/components/SocialCollection.vue
+++ src/components/SocialCollection.vue
@@ -6,7 +6,7 @@ import SocialIcon from './SocialIcon.vue'
     <div class="social-collection">
         <SocialIcon name="github" logo="/github.png" link="https://github.com/affq"></SocialIcon>
         <SocialIcon name="linkedin" logo="/in.png" link="https://www.linkedin.com/in/fabisiewicz"></SocialIcon>
-        <SocialIcon name="instagram" logo="/instagram.png" link="https://www.instagram.com/afabisiewicz"></SocialIcon>
+        <!-- <SocialIcon name="instagram" logo="/instagram.png" link="https://www.instagram.com/afabisiewicz"></SocialIcon> -->
         <SocialIcon name="gmail" logo="/gmail.png" link="mailto:adrianfabisiewicz@gmail.com"></SocialIcon>
     </div>
 </template>
